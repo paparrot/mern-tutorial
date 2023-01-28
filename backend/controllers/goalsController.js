fba953b8c6e0d79038b96700eec48044aef8a848
Klim <subbotin.klim@gmail.com>
2023-01-28 15:20:54 +0100

    add model handling to goals controller

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -1,21 +1,27 @@
 const asyncHandler = require('express-async-handler');
+const Goal = require('../models/goalModel');
 
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        msg: "Get goals"
-    })
+    const goals = await Goal.find();
+
+    res.status(200).json({goals});
 })
 
 // @desc Get goal
 // @route GET /api/goals/:id
 // @access Private
 const getGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        msg: "Show"
-    })
+    const goal = await Goal.findById(req.params.id);
+
+    if (! goal) {
+        res.status(404);
+        throw new Error('Goal not found.');
+    }
+
+    return res.status(200).json({goal})
 })
 
 // @desc Set goal
@@ -27,18 +33,30 @@ const createGoal = asyncHandler(async (req, res) => {
         throw new Error("Something went wrong.");
     }
 
-    return res.status(200)
-        .json({
-            msg: "Created"
-        })
+    const goal = await Goal.create({
+        text: req.body.text
+    });
+
+    return res.status(200).json({goal});
 })
 
 // @desc Delete goal
 // @route DELETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        msg: "Deleted"
+    const id = req.params.id;
+
+    const goal = await Goal.findById(id);
+
+    if (! goal) {
+        res.status(404);
+        throw new Error('Goal not found');
+    }
+
+    await Goal.findByIdAndDelete(id)
+
+    res.status(200).send({
+        message: "Successfully deleted"
     })
 })
 
@@ -46,9 +64,27 @@ const deleteGoal = asyncHandler(async (req, res) => {
 // @route PUT /api/goals/:id
 // @access Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        msg: "Updated"
-    })
+    if (! req.body.text) {
+        res.status(422);
+        throw new Error('Text field required.');
+    }
+
+    const id = req.params.id;
+
+    const goal = await Goal.findById(id);
+
+    if (! goal) {
+        res.status(404);
+        throw new Error("Goal not found.");
+    }
+
+    const updateGoal = await Goal.findByIdAndUpdate(id, {
+        text: req.body.text
+    }, {
+        new: true
+    });
+
+    res.status(200).send(updateGoal);
 })
 
 module.exports = {
