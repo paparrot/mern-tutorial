0cf39a599f66c3a8d6b861f1e1162daf18bb3df1
Klim <subbotin.klim@gmail.com>
2023-01-29 12:14:52 +0100

    add navigation to page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <h1>Here we go 👋</h1>
-    </div>
+      <>
+      <Router>
+          <div className='container'>
+              <Header></Header>
+              <Routes>
+                  <Route path="/" element={<Dashboard />}></Route>
+                  <Route path="/login" element={<Login />}></Route>
+                  <Route path="/register" element={<Register/>}></Route>
+              </Routes>
+          </div>
+      </Router>
+      </>
   );
 }
 
