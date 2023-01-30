92f8570e967a5d176b4e4ec46b2ae15bee8ea033
Klim <subbotin.klim@gmail.com>
2023-01-30 21:22:12 +0100

    prepare server to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -17,6 +18,14 @@ app.use(express.json())
 
 app.use('/api/v1/goals', protect, require('./routes/goalsRoute'))
 app.use('/api/v1/users', require('./routes/userRoute'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
+    })
+}
 
 app.use(errorHandler);
 app.listen(PORT, () => {
